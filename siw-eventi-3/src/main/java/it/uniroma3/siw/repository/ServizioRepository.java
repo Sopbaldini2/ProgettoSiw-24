ffc1d2770058a19f4472f479c3b5663384421e27
Sopbaldini2 <sop.baldini@stud.uniroma3.it>
2024-06-06 09:19:14 +0200

    Implemetazioni delle classi Servizio

---
+++ siw-eventi-3/src/main/java/it/uniroma3/siw/repository/ServizioRepository.java
@@ -0,0 +1,27 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import it.uniroma3.siw.model.Servizio;
+
+public interface ServizioRepository extends CrudRepository<Servizio,Long>{
+
+public boolean existsByNome(String nome);
+	
+	@Query(value="select * "
+			+ "from servizio s "
+			+ "where s.id not in "
+			+ "(select servizi_id "
+			+ "from evento_servizi "
+			+ "where evento_servizi.eventi_id = :eventoId)", nativeQuery=true)
+	public Iterable<Servizio> findServiziNotInEvento(@Param("eventoId") Long id);
+
+	@Modifying
+    @Query(value = "delete from evento_servizi where evento_servizi.servizi_id = :servizioId", nativeQuery=true)
+    void deleteReferencesInEventiServizi(@Param("servizioId") Long ingredienteId);
+
+}
+
---
+++ siw-eventi-3/src/main/java/it/uniroma3/siw/sevice/ServizioService.java
@@ -0,0 +1,45 @@
+package it.uniroma3.siw.sevice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.model.Servizio;
+//import it.uniroma3.siw.model.Servizio;
+import it.uniroma3.siw.repository.ServizioRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class ServizioService {
+	@Autowired
+	private ServizioRepository servizioRepository;
+	
+	public Servizio findById(Long id) {
+		return servizioRepository.findById(id).get();
+	}
+	
+	public Iterable<Servizio> findAll() {
+		return servizioRepository.findAll();
+	}
+	
+	public Iterable<Servizio> findServiziNotInEvento(@Param("eventoId") Long id){
+		 return servizioRepository.findServiziNotInEvento(id);
+	}
+
+	public void save(Servizio servizio) {
+		// TODO Auto-generated method stub
+		this.servizioRepository.save(servizio);
+	}
+
+	public boolean existsByNome(String nome) {
+		return servizioRepository.existsByNome(nome);
+	}
+	
+	
+	 @Transactional
+		public void deleteById(Long id) {
+			Servizio Servizio = findById(id);
+			servizioRepository.deleteReferencesInEventiServizi(id);
+			servizioRepository.delete(Servizio);
+		}
+}

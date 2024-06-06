534a8369a388b7692ca1f40ec58d6ade51a298e4
Sopbaldini2 <sop.baldini@stud.uniroma3.it>
2024-06-06 09:09:44 +0200

    Implemetazioni

---
+++ siw-eventi-3/src/main/java/it/uniroma3/siw/sevice/ClienteService.java
@@ -0,0 +1,59 @@
+package it.uniroma3.siw.sevice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.siw.model.Cliente;
+import it.uniroma3.siw.repository.ClienteRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The UserService handles logic for Users.
+ */
+@Service
+public class ClienteService {
+
+    @Autowired
+    protected ClienteRepository clienteRepository;
+
+    /**
+     * This method retrieves a User from the DB based on its ID.
+     * @param id the id of the User to retrieve from the DB
+     * @return the retrieved User, or null if no User with the passed ID could be found in the DB
+     */
+    @Transactional
+    public Cliente getUser(Long id) {
+        Optional<Cliente> result = this.clienteRepository.findById(id);
+        return result.orElse(null);
+    }
+
+    /**
+     * This method saves a User in the DB.
+     * @param user the User to save into the DB
+     * @return the saved User
+     * @throws DataIntegrityViolationException if a User with the same username
+     *                              as the passed User already exists in the DB
+     */
+    @Transactional
+    public Cliente saveUser(Cliente user) {
+        return this.clienteRepository.save(user);
+    }
+
+    /**
+     * This method retrieves all Users from the DB.
+     * @return a List with all the retrieved Users
+     */
+    @Transactional
+    public List<Cliente> getAllUsers() {
+        List<Cliente> result = new ArrayList<>();
+        Iterable<Cliente> iterable = this.clienteRepository.findAll();
+        for(Cliente user : iterable)
+            result.add(user);
+        return result;
+    }
+}

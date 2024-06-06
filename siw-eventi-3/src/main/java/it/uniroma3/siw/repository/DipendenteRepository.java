3b804416c8587177ef2178ac9a740353f6c57ae9
Sopbaldini2 <sop.baldini@stud.uniroma3.it>
2024-06-06 09:30:47 +0200

    Implementazione delle classi Dipendente

---
+++ siw-eventi-3/src/main/java/it/uniroma3/siw/repository/DipendenteRepository.java
@@ -0,0 +1,10 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.*;
+
+public interface DipendenteRepository extends CrudRepository<Dipendente,Long>{
+	boolean existsByNomeAndCognome(String nome, String cognome);
+
+}
---
+++ siw-eventi-3/src/main/java/it/uniroma3/siw/sevice/DipendenteService.java
@@ -0,0 +1,36 @@
+package it.uniroma3.siw.sevice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.model.Dipendente;
+import it.uniroma3.siw.repository.DipendenteRepository;
+
+@Service
+public class DipendenteService {
+
+	@Autowired
+	private DipendenteRepository dipendenteRepository;
+	
+	public void save(Dipendente dipendente) {
+		dipendenteRepository.save(dipendente);
+	}
+
+	public Dipendente findById(Long id) {
+		return dipendenteRepository.findById(id).get();
+	}
+
+	public Iterable<Dipendente> findAll() {
+		return dipendenteRepository.findAll();
+	}
+
+	public boolean existsByNomeAndCognome(String nome, String cognome) {
+		   return dipendenteRepository.existsByNomeAndCognome(nome,  cognome);
+	}
+
+	public void delete(Dipendente dipendente) {
+		dipendenteRepository.delete(dipendente);
+	}
+	 
+
+}

b01a0b415cc1fb8fa6a83775c158e4d804523596
vercatalano1 <ver.catalano1@stud.uniroma3.it>
2024-06-17 13:54:25 +0200

    piccola modifica index

--- siw-eventi-3/src/main/java/it/uniroma3/siw/controller/ClienteController.java
+++ siw-eventi-3/src/main/java/it/uniroma3/siw/controller/ClienteController.java
@@ -1,13 +1,18 @@
 package it.uniroma3.siw.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class ClienteController {
+	
 
 	@GetMapping("/cliente/indexUser")
-    public String indexUser() {
-        return "/cliente/indexUser.html";
-    }
+	public String indexCliente() {
+		return "/cliente/indexUser.html";
+	}
+	
+	
 }
